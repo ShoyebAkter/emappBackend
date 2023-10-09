6f37170aa0abd46b00309d61430568185726063b
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-09 14:58:16 +0600

    sendmail problem2

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const { MongoClient } = require('mongodb');
 const User = require("./routeHandler/user")
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://mp-app-eta.vercel.app',
-  // origin: 'http://localhost:5173',
+  // origin: 'https://mp-app-eta.vercel.app',
+  origin: 'http://localhost:5173',
 };
 app.use(cors(corsOptions));
 
