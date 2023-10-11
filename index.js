76897a5698e27bd0bb8f88dad98e8489f76646ef
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-11 13:38:07 +0600

    origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const { MongoClient } = require('mongodb');
 app.use(express.json());
 const corsOptions = {
-  origin: [ 'https://mp-app-eta.vercel.app','http://localhost:5173'],
+  origin: [ 'http://localhost:5173','https://mp-app-eta.vercel.app'],
 };
 app.use(cors(corsOptions));
 
