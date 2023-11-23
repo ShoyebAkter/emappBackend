ef0825908045351f46bc5f4672724fd7e5ac5828
Shoyeb Akter <heroreal5385@gmail.com>
2023-11-23 18:06:02 +0600

    cors

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const axios = require('axios');
 const nodemailer = require('nodemailer');
 const { MongoClient } = require('mongodb');
 app.use(express.json());
-// const corsOptions = {
-//   origin: ['https://mp-app-eta.vercel.app', 'http://localhost:5173'],
-// };
-app.use(cors());
+const corsOptions = {
+  origin: ['https://mp-app-eta.vercel.app', 'http://localhost:5173'],
+};
+app.use(cors(corsOptions));
 
 const dbName = 'emapp';
 const collectionName = 'orders';
