bb7201af86857ea99a133d120b5fd4b512fbbac1
Shoyeb <heroreal5385@gmail.com>
2024-02-13 11:19:51 +0600

    client category added

--- index.js
+++ index.js
@@ -297,6 +297,22 @@ app.get("/warehousepro/smallClient", async (req, res) => {
   }
 });
 
+//warehousepro client category
+app.get("/warehousepro/clientCategory", async (req, res) => {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("warehouseproClientCategory");
+
+    // Retrieve data from MongoDB
+    const data = await collection.find().toArray();
+
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 //post facebook post data
 app.post("/fbpost", async (req, res) => {
   const { id, uid, imageUrl, message, date } = req.body;
