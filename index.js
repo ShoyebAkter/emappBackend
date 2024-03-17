83e63a3dfc7a6199a9138705e7db35b0f6e5187d
Shoyeb <heroreal5385@gmail.com>
2024-03-17 22:28:55 +0600

    bodyparser

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(bodyParser.json()); 
 const dbName = "emapp";
 const collectionName = "orders";
 const client = new MongoClient(
