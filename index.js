26fa04406826869a0e03aece2eebf1e5dbe84fad
Shoyeb <heroreal5385@gmail.com>
2024-03-17 11:20:42 +0600

    remv serviceaccnt

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ admin.initializeApp({
 //   origin: 'http://localhost:5173/',
 // };
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(express.json());
 const dbName = "emapp";
 const collectionName = "orders";
@@ -33,6 +40,7 @@ async function connectToMongo() {
   }
 }
 connectToMongo();
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
