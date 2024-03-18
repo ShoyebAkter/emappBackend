8d07b2e5e10ae087564a45b14a2e79305a3b52b9
Shoyeb <heroreal5385@gmail.com>
2024-03-18 12:19:07 +0600

    ada2

--- index.js
+++ index.js
@@ -402,6 +402,20 @@ app.get("/warehousepro/mainData", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+app.get("/warehousepro/jsonData", async (req, res) => {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("warehouseproJsonData");
+
+    // Retrieve data from MongoDB
+    const data = await collection.find().toArray();
+
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 //warehousepro state data
 app.get("/warehousepro/stateData", async (req, res) => {
   try {
